9cff2c5b3eb6eef58e60cc69c46cd9f4468cfe62
mikethebro <xenophon.of.athens@gmail.com>
2018-01-25 13:55:08 -0700

    adding templates and static files to server

--- server.py
+++ server.py
@@ -2,10 +2,10 @@ import tornado.ioloop
 import tornado.web
 import tornado.httpserver
 import os
+import config
 
 import mathfile
 
-t = tornado.template.Template("<html><h3>{{ myvalue }}</h3></html>")
 
 boring_stuff = """Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
 
@@ -15,12 +15,23 @@ The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for t
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
     	result = mathfile.do_math(8, 11)
-    	self.render("template.html", myvalue="Boring Title", text=result)
+    	self.render("template.html", text=result)
+
+class Server(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+            # Add more paths here
+        ]
+        settings = {
+        "debug": True,
+        "template_path": os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates"),
+        "static_path": os.path.join(os.path.dirname(os.path.abspath(__file__)), "static")
+        }
+        tornado.web.Application.__init__(self, handlers, **settings)
 
 def main():
-    application = tornado.web.Application([
-        (r"/", MainHandler),
-    ])
+    application = Server()
     http_server = tornado.httpserver.HTTPServer(application)
     port = int(os.environ.get("PORT", 5000))
     http_server.listen(port)
